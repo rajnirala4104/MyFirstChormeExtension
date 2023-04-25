9228190da7cb21f1a413ae682db7cbe8646f1dd1
Raj_Nirala <83572606+rajnirala4104@users.noreply.github.com>
2023-04-25 23:51:30 +0530

    organised the code

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log("this is raj and i'm gonna create my first chrome extensoin");
+chrome.tabs.onUpdated.addListener((tabId, tab)=>{
+    if(tab.url && tab.url.includes("geeksforgeeks.org/*")){
+        const articaleTitle = document.querySelector('.gfg_home_page_search_heading')   
+        articaleTitle.innerHTML = `<h1>This is Raj Nirala</h1>`;
+    }
+})
